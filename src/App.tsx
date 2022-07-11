38a3be91c34380549afbb016bc1c44076c305eee
Tobias Gänzler <1056318+tobias-gaenzler@users.noreply.github.com>
2022-07-11 21:33:49 +0200

    Use strict mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelpView, PracticeView, PlayView, NotFoundView } from "./views";
 import { ModelContextProvider, UserContextProvider } from "./common";
 import { Header } from "./components";
+import { StrictMode } from "react";
 
 function App() {
   return (
-    <UserContextProvider>
-      <ModelContextProvider>
-        <BrowserRouter>
-          <Header />
-          <AppRoutes />
-        </BrowserRouter>
-      </ModelContextProvider>
-    </UserContextProvider>
+    <StrictMode>
+      <UserContextProvider>
+        <ModelContextProvider>
+          <BrowserRouter>
+            <Header />
+            <AppRoutes />
+          </BrowserRouter>
+        </ModelContextProvider>
+      </UserContextProvider>
+    </StrictMode>
   );
 }
 
