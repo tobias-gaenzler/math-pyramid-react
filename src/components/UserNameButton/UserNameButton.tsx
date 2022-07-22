77db42e0ef76b6417f626f3ce8b1f92ae12296c6
Tobias Gänzler <1056318+tobias-gaenzler@users.noreply.github.com>
2022-07-22 21:39:51 +0200

    Extract user name dialog

--- src/components/UserNameButton/UserNameButton.tsx
+++ src/components/UserNameButton/UserNameButton.tsx
@@ -1,38 +1,17 @@
 import { AccountCircle } from "@mui/icons-material";
 import {
-  Dialog,
-  DialogContent,
-  TextField,
-  InputAdornment,
-  DialogActions,
-  Button,
   IconButton,
   Typography,
 } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { useUserContext } from "../../common";
+import { UserNameDialog } from "./UserNameDialog";
 
 type Props = {};
 const UserNameButton: React.FC<Props> = () => {
-  const { userName, saveUserName } = useUserContext();
+  const { userName } = useUserContext();
   const [dialogOpen, setDialogOpen] = useState<boolean>(false);
 
-  const userNameField = useRef<HTMLInputElement>();
-  const closeDialog = () => { setDialogOpen(false) }
-  const handleUserNameInput = () => {
-    const currentValue = userNameField?.current?.value
-    // not blank
-    if (currentValue && !/^\s*$/.test(currentValue)) {
-      saveUserName(currentValue);
-    }
-    setDialogOpen(false);
-  };
-  useEffect(() => {
-    // fix focus on input field after dialog opens
-    window.setTimeout(function () {
-      userNameField?.current?.focus();
-    }, 200);
-  });
   return (
     <>
       <IconButton
@@ -48,30 +27,7 @@ const UserNameButton: React.FC<Props> = () => {
           {userName}
         </Typography>
       </IconButton>
-      <Dialog open={dialogOpen} onClose={closeDialog}>
-        <DialogContent>
-          <TextField
-            inputRef={userNameField}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <AccountCircle />
-                </InputAdornment>
-              ),
-            }}
-            id="userName"
-            label="User name"
-            onKeyPress={(event) => {
-              if (event.key === "Enter") {
-                handleUserNameInput();
-              }
-            }}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleUserNameInput}>Save</Button>
-        </DialogActions>
-      </Dialog>
+      <UserNameDialog open={dialogOpen} setDialogOpen={setDialogOpen} />
     </>
   );
 };
